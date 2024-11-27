efa82d6b53fea1ebfdc2b6ed07c1216b4fd21000
DongBai123 <3257039675@qq.com>
2024-11-27 21:23:12 +0800

    test: Pass move command test cases

--- cpp-training-start (1)/cpp-training/src/Executorlmpl.cpp
+++ cpp-training-start (1)/cpp-training/src/Executorlmpl.cpp
@@ -9,9 +9,70 @@ namespace adas
     {
         
     }
-    void Executorlmpl::Execute(const std::string& coomands)noexcept
+    void Executorlmpl::Execute(const std::string& commands)noexcept
     {
-
+        for (auto order=commands.begin();order<commands.end();order++)
+    {
+    //前进代码
+    if (*order == 'M')
+    {
+        if (pose.heading == 'N')
+        {
+            pose.y++;
+        }
+        else if (pose.heading == 'S')
+        {
+            pose.y--;
+        }
+        else if (pose.heading == 'E')
+        {
+            pose.x++;
+        }
+        else if (pose.heading == 'W')
+        {
+            pose.x--;
+      }
+    }
+    else if (*order == 'L')//左转代码
+    {
+        if (pose.heading == 'N')
+        {
+            pose.heading ='W';
+        }
+        else if (pose.heading == 'S')
+        {
+            pose.heading = 'E';
+        }
+        else if (pose.heading == 'E')
+        {
+            pose.heading = 'N';
+        }
+        else if (pose.heading == 'W')
+        {
+            pose.heading = 'S';
+        }
+    }
+    else if (*order == 'R')//右转代码
+    {
+        if (pose.heading == 'N')
+        {
+            pose.heading = 'E';
+        }
+        else if (pose.heading == 'S')
+        {
+            pose.heading = 'W';
+        }
+        else if (pose.heading == 'E')
+        {
+            pose.heading = 'S';
+        }
+        else if (pose.heading == 'W')
+        {
+            pose.heading = 'N';
+        }
+    }
+   
+    }
     }
     Pose Executorlmpl::Query()const noexcept{
         return pose;
